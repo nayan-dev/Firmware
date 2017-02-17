816258c7e1f431c5ed42f49a1fd470c639ef2eaa
Nikhil Upadhye <nikhil@aus.co.in>
2017-02-18 04:30:04 +0530

    spv2 flight ready

--- src/drivers/px4fmu/fmu.cpp
+++ src/drivers/px4fmu/fmu.cpp
@@ -325,7 +325,13 @@ const PX4FMU::GPIOConfig PX4FMU::_gpio_tab[] = {
 	{GPIO_GPIO2_INPUT,       GPIO_GPIO2_OUTPUT,       0},
 	{GPIO_GPIO3_INPUT,       GPIO_GPIO3_OUTPUT,       0},
 	{GPIO_GPIO4_INPUT,       GPIO_GPIO4_OUTPUT,       0},
-//	{GPIO_GPIO5_INPUT,       GPIO_GPIO5_OUTPUT,       0},
+	{GPIO_GPIO5_INPUT,       GPIO_GPIO5_OUTPUT,       0},
+	{GPIO_GPIO6_INPUT,       GPIO_GPIO6_OUTPUT,       0},
+	{GPIO_GPIO7_INPUT,       GPIO_GPIO7_OUTPUT,       0},
+	{GPIO_GPIO8_INPUT,       GPIO_GPIO8_OUTPUT,       0},
+	{GPIO_GPIO9_INPUT,       GPIO_GPIO9_OUTPUT,       0},
+
+
 
 //	{0,                      GPIO_VDD_5V_PERIPH_EN,   0},
 //	{0,                      GPIO_VDD_3V3_SENSORS_EN, 0},
