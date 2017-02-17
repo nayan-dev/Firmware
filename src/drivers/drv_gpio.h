2b50806f4712de9b519af666034b42ad21f28cb9
Nikhil Upadhye <nikhil@aus.co.in>
2017-02-18 04:30:04 +0530

    spv2 flight ready

--- src/drivers/drv_gpio.h
+++ src/drivers/drv_gpio.h
@@ -133,7 +133,7 @@
 # define GPIO_SERVO_3		(1<<2)		/**< servo 3 output */
 # define GPIO_SERVO_4		(1<<3)		/**< servo 4 output */
 # define GPIO_SERVO_5		(1<<4)		/**< servo 5 output */
-# define GPIO_SERVO_6		(1<<5)		/**< servo 6 output */
+//# define GPIO_SERVO_6		(1<<5)		/**< servo 6 output */
 
 //# define GPIO_5V_PERIPH_EN	(1<<6)		/**< PA8 - !VDD_5V_PERIPH_EN */
 //# define GPIO_3V3_SENSORS_EN	(1<<7)		/**< PE3 - VDD_3V3_SENSORS_EN */
